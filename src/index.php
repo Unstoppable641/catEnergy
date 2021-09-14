a8ad24ef883fe6008e566e818c6d74e5742c48e4
webking_nch <car641@mail.ru>
2021-09-14 23:51:11 +0400

    added src folder

---
+++ src/index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Cat Energy</title>
+    <?php include 'head.php' ?>
+
+</head>
+<body>
+    <?php include 'nav.php' ?>
+    <main>
+        <section class="header-block">
+            <div class="container">
+                <div class="header-block-wrap">
+                    <h2 class="header-title">Функциональное питание для котов</h2>
+                    <p class="header-text">Занялся собой? Займись котом!</p>
+                    <img src="img/index-can.png" alt="" class="header-img">
+                    <a href="/program.php" class="button header-button">Подобрать программу</a>
+                </div>
+            </div>
+        </section>
+        <section class="program-list">
+            <div class="container">
+                <div class="program-list-wrap">
+                    <div class="program-list-wrap__item">
+                        <div class="program-list-wrap__item-img">
+                            <svg class="program-icon program-icon_slim" xmlns="http://www.w3.org/2000/svg" width="35" height="49" viewBox="0 0 70 97"><path d="M54.8 0h-.4c-.7.4-1.1.9-1.4 1.6-.6 1.1-1 2.3-1.3 3.4-.2.8-.7 1.2-1.5 1.3-2.3.4-4 1.7-5.2 3.7-.7 1.3-1.3 2.6-1.8 3.9-1.4 3.7-2.5 7.5-3.8 11.2-.7 1.9-.8 1.9-2.7 1.9-2.3 0-2.1-.4-2.1 2.1 0 .8.2 1.2 1.1 1.2.3 0 .7-.1.8.2.1.3-.2.5-.4.7-1.2 1.3-2.7 2-4.3 2.4-1.9.5-3.8.9-5.7 1.5-4.7 1.6-9.1 3.8-12.6 7.6-2.1 2.3-3.5 5-4.7 7.8-3.8 8.7-5.6 17.8-7 27-.6 4.3-1.1 8.6-1.5 13C.2 92.7 0 94.8 0 97h33.3c.1-.5-.1-.9-.2-1.3-.7-1.8-2.1-2.7-3.8-3.1-.7-.2-.8-.4-.4-.9.7-.9 1.2-2 1.6-3.1 1.6-4.8 2.3-9.8 3.1-14.8.6-3.8 1.5-7.4 3.4-10.8 1.5-2.5 3.3-4.6 5.6-6.4.8-.6.9-.6 1 .4.1.7.1 1.4.2 2.2.3 4.7.3 9.5-.2 14.2-.3 3.7-.5 7.4-.2 11 .4 4.3 1.2 8.4 2.2 12.6h13.3c-.3-2.2-2.1-4-4.2-4.4-1.3-.2-2.5-.1-3.8.2-.6.1-.8 0-.9-.6-.4-5.3-.1-10.6 1-15.8 1.3-6 2.4-12.1 3.1-18.2.3-2.5 1-4.8 2.3-7 1.2-2.1 2.2-4.3 3.4-6.5.9-1.7 1.1-3.4.4-5.2-.3-.7-.5-1.4-.9-2.1-1-2-2-3.9-2.9-5.9-.5-1-.4-1.1.7-1.1 1.6 0 1.6 0 1.7-1.6v-.7c0-.7-.3-1.1-1-1h-1.5c-.7 0-.9-.3-.7-1 .6-2.3 2.8-4.1 5.1-4.4 2.2-.3 4.4 0 6.6.4.6.1 1 0 1.3-.6.4-1 .8-2 1.3-3 .2-.4.2-.8-.1-1.1-3.2-4.2-6.7-8-11.4-10.4-.5-.3-.8-.6-.9-1.1-.3-1.4-.7-2.8-1.3-4.2-.3-.7-.6-1.3-1.4-1.7z"/></svg>
+                        </div>
+                        <h2 class="program-list-wrap__item-title">похудение</h2>
+                        <p class="program-list-wrap__item-text">Ваш кот весит больше собаки и почти утратил способность
+                            лазить по деревьям? Пора на диету! Cat Energy Slim поможет
+                            вашему питомцу сбросить лишний вес.</p>
+                        <div class="program-list-wrap__item-link">
+                            <a href="" class="arrow-link">каталог slim <svg class="program-arrow" width="24" height="12" viewBox="0 0 24 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line y1="6" x2="18" y2="6" stroke="black" stroke-width="2"/>
+                                    <path d="M24 6L17.25 11.1962L17.25 0.803848L24 6Z" fill="black"/>
+                                </svg></a>
+                        </div>
+                    </div>
+                    <div class="program-list-wrap__item">
+                        <div class="program-list-wrap__item-img">
+                            <svg class="program-icon program-icon_pro" width="134" height="56" viewBox="0 0 134 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0)">
+                                    <path d="M123.1 5.80001C124.254 3.96309 125.789 2.39461 127.6 1.20001C128.6 0.500006 129.2 0.500006 129.8 1.80001C130.6 3.70001 131.3 5.70001 130.7 7.80001C130.591 8.15618 130.57 8.5336 130.64 8.89955C130.71 9.26549 130.867 9.6089 131.1 9.90001C131.7 10.7 132.2 11.5 132.7 12.2C133.38 13.0638 133.822 14.0913 133.98 15.1795C134.138 16.2678 134.007 17.3784 133.6 18.4C133.233 19.3918 133.163 20.4692 133.4 21.5C133.6 22.49 133.6 23.51 133.4 24.5C133.319 25.2414 133.019 25.9419 132.538 26.5119C132.057 27.0819 131.417 27.4956 130.7 27.7C129.421 27.9434 128.279 28.6569 127.5 29.7C127.405 29.8536 127.273 29.9803 127.115 30.0679C126.958 30.1555 126.78 30.201 126.6 30.2C125.4 30.4 122.4 27.7 122.4 26.5V25.9C122.5 22.3 122.3 22 119 20.5C118.309 20.2564 117.69 19.8439 117.2 19.3C116.6 18.5 115.8 18.6 114.9 18.9C114 19.2 113.5 19.8 113.6 20.8C113.901 23.2209 114.002 25.6626 113.9 28.1C113.907 28.3782 113.975 28.6514 114.1 28.9C117.2 33.4 118.4 38.7 119.9 43.8C121.1 47.7 123.3 49.8 127.3 50.1C127.94 50.1511 128.575 50.2514 129.2 50.4C130.022 50.532 130.785 50.9076 131.391 51.478C131.997 52.0485 132.418 52.7877 132.6 53.6C133.3 55.7 132.9 56.2 130.8 56.2H124.3C123.458 56.2426 122.633 55.9556 122 55.4C120.988 54.5459 119.838 53.8696 118.6 53.4C117.231 52.8947 116.046 51.9887 115.2 50.8C114.029 49.1635 112.688 47.6551 111.2 46.3C108.4 43.4 105.4 40.6 102.8 37.6C100.821 35.5558 99.6789 32.8443 99.5999 30C99.5999 29.1 99.3999 28.7 98.3999 29.1C98.1005 29.29 97.7545 29.3938 97.3999 29.4C94.5999 29.4 93.1999 31.1 92.1999 33.6C91.2999 35.7 90.0999 37.7 88.9999 39.8C88.3762 41.1145 88.0355 42.5454 87.9999 44C87.9999 46.4 88.5999 47.2 90.7999 48C92.1999 48.5 93.4999 49 94.1999 50.4C94.9999 52 94.7999 52.4 93.0999 52.5H86.2999C85.9079 52.5388 85.5136 52.4535 85.1727 52.2562C84.8317 52.0588 84.5614 51.7593 84.3999 51.4L82.5999 48.4C82.0693 47.6172 81.7296 46.721 81.6081 45.7832C81.4865 44.8454 81.5865 43.8922 81.8999 43C82.3327 41.2635 82.3669 39.4516 81.9999 37.7C81.7478 36.4801 81.7696 35.2195 82.0635 34.009C82.3575 32.7986 82.9164 31.6684 83.6999 30.7C84.2999 30 84.2999 29.7 83.2999 29.5C79.2108 28.6314 75.2466 27.254 71.4999 25.4C71.4734 25.4 71.448 25.3895 71.4292 25.3707C71.4105 25.352 71.3999 25.3265 71.3999 25.3C68.1999 24.1 67.5999 24.3 66.6999 27.6C65.8999 30.4 65.0999 33.2 64.4999 36.1C63.7999 39.3 64.5999 42.3 65.5999 45.3C65.9999 46.5 67.1999 46.4 68.0999 46.7C70.0999 47.3 71.7999 48.1 72.5999 50.2C73.2999 51.8 73.1999 52 71.4999 52C69.7999 52 68.0999 51.9 66.2999 52C64.781 52.0275 63.2985 51.5334 62.0999 50.6C57.2999 47.4 55.8999 42.5 55.4999 37.2C55.2999 33.9 53.4999 31.3 51.3999 28.8C50.9999 28.3 50.5999 28.1 49.9999 28.5C47.8534 29.6836 45.6124 30.687 43.2999 31.5C38.4742 33.204 34.2395 36.2585 31.0999 40.3C29.9276 41.775 29.1683 43.5351 28.8999 45.4C28.5999 47.2 29.0999 48 30.8999 48.5C32.6999 49 33.5999 50 33.7999 51.9C33.8999 52.7 33.5999 53.1 32.7999 53.1H24.6999C24.4348 53.0812 24.1782 52.9987 23.9518 52.8594C23.7255 52.7201 23.5362 52.5282 23.3999 52.3C22.1438 50.7448 21.4716 48.799 21.4999 46.8C21.4474 43.3449 21.749 39.8936 22.3999 36.5C22.5334 35.4837 22.9128 34.5153 23.5053 33.6788C24.0978 32.8424 24.8855 32.1631 25.7999 31.7C31.6999 28.9 34.7999 23.6 37.5999 18.1C38.1999 17 37.6999 16.8 36.6999 16.9C33.9013 17.2885 31.2496 18.3905 28.9999 20.1C26.0541 22.2488 23.2791 24.6226 20.6999 27.2C17.9496 29.9937 14.3026 31.7286 10.3999 32.1C3.09992 32.5 -0.300081 28.4 -8.06041e-05 21.8C0.0669819 19.5496 0.915567 17.3928 2.39992 15.7C2.91594 15.031 3.61965 14.531 4.42119 14.2638C5.22272 13.9966 6.0857 13.9744 6.89992 14.2C8.39992 14.6 8.79992 15.4 8.19992 16.9C7.89992 17.5 7.49992 18 7.19992 18.6C6.76852 19.213 6.43144 19.8871 6.19992 20.6C5.09992 23.6 6.59992 25.7 9.69992 26C12.9999 26.3 15.6999 24.9 18.1999 22.9C21.2118 20.4882 23.9006 17.6986 26.1999 14.6C27.9192 12.3374 30.084 10.4513 32.5607 9.05813C35.0375 7.66497 37.7735 6.79434 40.5999 6.50001C42.8999 6.20001 44.8999 5.20001 47.0999 4.60001C51.2144 3.34561 55.4989 2.73836 59.7999 2.80001C61.792 2.88297 63.7765 2.5066 65.5999 1.70001C70.4999 -0.299994 75.5999 -0.299994 80.6999 0.700006C84.6999 1.50001 88.6999 2.50001 92.5999 3.50001C94.9209 3.93248 97.3074 3.8643 99.5999 3.30001C103.989 2.18754 108.478 1.51753 113 1.30001C115.962 1.15587 118.915 1.73954 121.6 3.00001C123.6 3.90001 123.6 3.90001 123.1 5.80001Z" fill="black"/>
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0">
+                                        <rect width="134" height="56" fill="white"/>
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                        <h2 class="program-list-wrap__item-title">набор массы</h2>
+                        <p class="program-list-wrap__item-text">Заработать авторитет среди дворовых котов и даже собак?
+                            Серия Cat Energy Pro поможет вашему коту нарастить
+                            необходимые мышцы!</p>
+                        <div class="program-list-wrap__item-link">
+                            <a href="" class="arrow-link">каталог pro <svg class="program-arrow" width="24" height="12" viewBox="0 0 24 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line y1="6" x2="18" y2="6" stroke="black" stroke-width="2"/>
+                                    <path d="M24 6L17.25 11.1962L17.25 0.803848L24 6Z" fill="black"/>
+                                </svg></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="how-it-work">
+            <div class="container">
+                <h2 class="how-it-work-title">Как это работает</h2>
+                <div class="how-it-work-wrap">
+                    <div class="how-it-work-wrap__item">
+                        <div class="how-it-work-wrap__item-info">
+                            <div class="img"><img src="img/icon_leaf.svg" alt=""></div>
+                            <div class="text">Функциональное питание содержит только полезные питательные вещества</div>
+                        </div>
+                        <div class="how-it-work-wrap__item-number">1</div>
+                    </div>
+                    <div class="how-it-work-wrap__item">
+                        <div class="how-it-work-wrap__item-info">
+                            <div class="img"><img src="img/icon_tub.svg" alt=""></div>
+                            <div class="text">Выпускается в виде порошка, который нужно лишь залить кипятком и готово.</div>
+                        </div>
+                        <div class="how-it-work-wrap__item-number">2</div>
+                    </div>
+                    <div class="how-it-work-wrap__item">
+                        <div class="how-it-work-wrap__item-info">
+                            <div class="img"><img src="img/icon_eat.svg" alt=""></div>
+                            <div class="text">Замените один-два приема обычной еды на наше функциональное питание.</div>
+                        </div>
+                        <div class="how-it-work-wrap__item-number">3</div>
+                    </div>
+                    <div class="how-it-work-wrap__item">
+                        <div class="how-it-work-wrap__item-info">
+                            <div class="img"><img src="img/icon_alarm.svg" alt=""></div>
+                            <div class="text">Уже через месяц наслаждайтесь изменениями к лучшему вашего питомца!</div>
+                        </div>
+                        <div class="how-it-work-wrap__item-number">4</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="demo">
+            <div class="container">
+                <div class="demo-wrap">
+                    <div class="demo-wrap-info">
+                        <h2 class="demo-wrap-info__title">Живой пример</h2>
+                        <p class="demo-wrap-info__text">Борис сбросил 5 кг за 2 месяца, просто заменив свой обычный корм на Cat Energy Slim. Отличный результат без изнуряющих тренировок! При этом он не менял своих привычек и по-прежнему спит по 16 часов в день.</p>
+                        <div class="demo-wrap-info__stats">
+                            <div class="demo-stats-wrap">
+                                <div class="demo-stats-wrap__item demo-stats-wrap__item_weight">
+                                    <span class="value">5 кг</span>
+                                    <span class="title">снижение веса</span>
+                                </div>
+                                <div class="demo-stats-wrap__item demo-stats-wrap__item_days">
+                                    <span class="value">60 дней</span>
+                                    <span class="title">затрачено времени</span>
+                                </div>
+                            </div>
+                            <div class="demo-price">
+                                <span>Затраты на питание: </span>
+                                <span>15 000 руб.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="demo-wrap-slider">
+                        <div class="demo-wrap-slider-image-wrap">
+                            <div class="demo-wrap-slider-image-wrap__img demo-wrap-slider-image-wrap__img_before"></div>
+                            <div class="demo-wrap-slider-image-wrap__img demo-wrap-slider-image-wrap__img_after"></div>
+                        </div>
+                        <div class="demo-wrap-slider-buttons">
+                            <button class="demo-btn demo-btn_before" type="button">Было</button>
+                            <div class="demo-progress">
+                                <input type="range" min="1" max="100" value="50" class="demo-range">
+                                <div class="demo-progress-toggle"></div>
+                            </div>
+                            <button class="demo-btn demo-btn_after" type="button">Стало</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="demo-wrap-bg"></div>
+            </div>
+        </section>
+        <section class="contacts">
+            <div class="container">
+                <div class="contacts-info-wrap">
+                    <h2 class="contacts-info-wrap__title">Приглашаем<br> к сотрудничеству дилеров!</h2>
+                    <div class="contacts-info-wrap__adress">
+                        <div class="adress-street">ул. Большая <br>
+                            Конюшенная, д. 19/8</div>
+                        <div class="adress-city">Санкт-Петербург</div>
+                    </div>
+                </div>
+                <div class="contacts-map" id="map"></div>
+            </div>
+        </section>
+    </main>
+    <?php include 'footer.php' ?>
+    <?php include 'scripts.php' ?>
+</body>
+</html>
